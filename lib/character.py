214705b93f53b8676223ce1510c77244f4d7ba47
Michael Fiano <axedcode@gmail.com>
2009-10-10 14:02:41 -0400

    Forgot to fix base character class before commiting the last changes.

--- lib/character.py
+++ lib/character.py
@@ -2,7 +2,7 @@ class BaseCharacter(object):
     """The base class from which all other game characters derive."""
 
     def __init__(self, name, hp, hp_max, mp, mp_max, attack, defense, spells,
-            items, exp, gold, size, movement, num_attacks, damage, morale):
+            items, exp, gold):
         self.name = name
         self.hp = hp
         self.hp_max = hp_max
@@ -36,7 +36,7 @@ class NonPlayerCharacter(BaseCharacter):
     """A computer-controlled character."""
 
     def __init__(self, name="NPC", hp=1, hp_max=1, mp=0, mp_max=0, attack=1,
-            defense=1, spells=[], items=[], exp, gold=0, frequency=0, size=2,
+            defense=1, spells=[], items=[], exp=0, gold=0, frequency=0, size=2,
             movement=1, num_attacks=1, damage=1, morale=5):
         BaseCharacter.__init__(self, name, hp, hp_max, mp, mp_max, attack,
             defense, spells, items, exp, gold)
