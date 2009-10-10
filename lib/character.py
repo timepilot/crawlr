843e8ea236c903349be8da90adcafd7c4f061546
Michael Fiano <axedcode@gmail.com>
2009-10-10 13:53:07 -0400

    Added a base non-player character class.

--- lib/character.py
+++ lib/character.py
@@ -1,8 +1,8 @@
-class BaseCharacter:
+class BaseCharacter(object):
     """The base class from which all other game characters derive."""
 
     def __init__(self, name, hp, hp_max, mp, mp_max, attack, defense, spells,
-            items):
+            items, exp, gold, size, movement, num_attacks, damage, morale):
         self.name = name
         self.hp = hp
         self.hp_max = hp_max
@@ -12,16 +12,17 @@ class BaseCharacter:
         self.defense = defense
         self.spells = spells
         self.items = items
+        self.exp = exp
+        self.gold = gold
 
 
 class PlayerCharacter(BaseCharacter):
     """The hero of the game."""
 
     def __init__(self, name="Hero", hp=10, hp_max=10, mp=0, mp_max=0,
-        attack=1, defense=1, spells=[], items=[]):
+            attack=1, defense=1, spells=[], items=[], exp=0, gold=0):
         BaseCharacter.__init__(self, name, hp, hp_max, mp, mp_max, attack,
-            defense, spells, items)
-        self.exp = 0
+            defense, spells, items, exp, gold)
         self.exp_max = 1000
         self.equipment = {
             'ArmorBody': '', 'ArmorHead': '', 'ArmorHands': '',
@@ -29,3 +30,19 @@ class PlayerCharacter(BaseCharacter):
             'RingRight': '', 'BraceletLeft': '', 'BraceletRight': '',
             'Necklace': '', 'WeaponLeft': '', 'WeaponRight': '',
             'Items': items }
+
+
+class NonPlayerCharacter(BaseCharacter):
+    """A computer-controlled character."""
+
+    def __init__(self, name="NPC", hp=1, hp_max=1, mp=0, mp_max=0, attack=1,
+            defense=1, spells=[], items=[], exp, gold=0, frequency=0, size=2,
+            movement=1, num_attacks=1, damage=1, morale=5):
+        BaseCharacter.__init__(self, name, hp, hp_max, mp, mp_max, attack,
+            defense, spells, items, exp, gold)
+        self.frequency = frequency
+        self.size = size
+        self.movement = movement
+        self.num_attacks = num_attacks
+        self.damage = damage
+        self.morale = morale
