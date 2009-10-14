a0389b1dfe48418bc5e12a193e3bf9334c8fce57
Michael Fiano <axedcode@gmail.com>
2009-10-13 23:28:58 -0400

    Forgot to delete unused files.

--- lib/monster.py
+++
@@ -1,8 +0,0 @@
-from sprite import *
-from character import *
-
-class Monster(MonsterSprite, NonPlayerCharacter):
-
-    def __init__(self, scene):
-        MonsterSprite.__init__(self, scene)
-        NonPlayerCharacter.__init__(self, "NPC")
--- lib/player.py
+++
@@ -1,9 +0,0 @@
-from sprite import *
-from character import *
-
-class Player(PlayerSprite, PlayerCharacter):
-    """The main player character."""
-
-    def __init__(self, scene):
-        PlayerSprite.__init__(self, scene)
-        PlayerCharacter.__init__(self, 'Hero')
