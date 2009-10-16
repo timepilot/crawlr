3dac877bf7d26420b563fdde493e9285e754cbc2
Michael Fiano <axedcode@gmail.com>
2009-10-16 01:48:36 -0400

    Need to rethink monster generator.

--- lib/battle.py
+++ lib/battle.py
@@ -13,19 +13,20 @@ class Battle(object):
         self.window = scene.window
         self.region = int(scene.player.current_region)
         self.monster_quantity = {}
+        self.monsters = MONSTER_DICT[self.region]
         self.monster_list = []
 
     def create_monsters(self):
         """Create a random list of monsters from the current region number."""
 
         # Create a dictionary for the amount of each type of monster.
-        for monster in MONSTER_DICT[self.region]:
+        for monster in self.monsters:
             self.monster_quantity[monster] = 0
 
         # Pick monsters randomly while staying within the maximum allowed.
         times = 0
         while times < Die(MAX_MONSTERS).roll():
-            monster = choice(MONSTER_DICT[self.region])
+            monster = choice(self.monsters)
             if self.monster_quantity[monster] < monster(self).max_amount:
                 self.monster_list.append(monster)
                 self.monster_quantity[monster] += 1
