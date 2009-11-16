ad53fc78b3ff7455c210b2f05d3bdcf992490946
Michael Fiano <axedcode@gmail.com>
2009-11-16 15:32:58 -0500

    Began work on a state machine to handle the different states the game may be in.

--- lib/game.py
+++ lib/game.py
@@ -3,7 +3,7 @@ from sys import exit
 import pygame
 from pygame.locals import *
 from constants import *
-from scene import Scene
+from states import *
 
 class Game(object):
     """Creates the game and manages the main loop."""
@@ -13,7 +13,6 @@ class Game(object):
 
         environ['SDL_VIDEO_CENTERED'] = '1'
         pygame.init()
-        self.running = True
         self.fullscreen = False
         self.window = pygame.display.set_mode(WINDOW_SIZE, self.fullscreen,
             COLOR_DEPTH)
@@ -21,74 +20,6 @@ class Game(object):
         pygame.mouse.set_visible(False)
 
     def run(self):
-        """Runs the main game loop."""
+        state = GameState(self.window)
+        state.run()
 
-        # Create the main game objects.
-        self.clock = pygame.time.Clock()
-        self.scene = Scene(self.window, 1)
-
-        # For each frame, check for input and redraw accordingly.
-        while self.running:
-            self.clock.tick(FRAME_RATE)
-            self.check_events()
-            self.scene.draw()
-            self.show_debug()
-
-    def check_events(self):
-        """Check for user input in the game."""
-
-        self.move_keys = self.scene.player.move_keys
-        for event in pygame.event.get():
-            if event.type == QUIT: self.exit
-            elif event.type == KEYDOWN:
-                key_name = pygame.key.name
-                key = event.key
-                if event.key == K_ESCAPE: self.exit()
-                elif event.key == K_f: pygame.display.toggle_fullscreen()
-                elif event.key == K_n: self.scene.reload(self)
-                elif event.key == K_d: self.scene.toggle_dialog()
-                elif event.key in (K_DOWN, K_UP, K_LEFT, K_RIGHT):
-                    self.player_input(True, key_name, key)
-            elif event.type == KEYUP:
-                key_name = pygame.key.name
-                key = event.key
-                if event.key in (K_DOWN, K_UP, K_LEFT, K_RIGHT):
-                    self.player_input(False, key_name, key)
-
-    def player_input(self, moving, name, key):
-        """Controls key input to the player character."""
-
-        if moving:
-            self.move_keys.append(name(key))
-            self.scene.player.direction = self.move_keys[-1]
-            self.scene.player.stop = False
-        else:
-            if len(self.move_keys) > 0:
-                keyid = self.move_keys.index(name(key))
-                del self.move_keys[keyid]
-                if len(self.move_keys) != 0:
-                    self.scene.player.direction = (self.move_keys[-1])
-                else: self.scene.player.stop = True
-
-    def show_debug(self):
-        """Print debugging info to console."""
-
-        if SHOW_FRAME_RATE:
-            print 'Framerate: %f/%f' % (int(self.clock.get_fps()), FRAME_RATE)
-        if SHOW_RECTS:
-            self.scene.map.layers['terrain'].image.fill(
-                (0,0,0), self.scene.player.collide_rect)
-            for rect in (self.scene.map.nowalk):
-                self.scene.map.layers['terrain'].image.fill(
-                    (255,255,255), rect)
-        if SHOW_TERRAIN:
-            print "Current terrain: " + (
-                self.scene.player.current_terrain)
-        if SHOW_REGION:
-            print "Current region: " + (
-                self.scene.player.current_region)
-
-    def exit(self):
-        """Exits the game."""
-
-        self.running = False
